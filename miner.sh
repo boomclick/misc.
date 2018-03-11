4edfc385eff34d1e4589e29985d512a2fd02a95a
boomclick <36665684+boomclick@users.noreply.github.com>
2018-03-11 09:58:43 -0400

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,31 +2,41 @@
 #mining pool selector and command abbreviator
 #© 2018. This program is released into the public domain.
 
-printf "welcome to EZscrytpminer!\\n"
-printf "what will you be mining today?\\n1.)digibyte\\n2.)lynx\\n3.)artbyte\\n"
-read -r CURRENCY
-printf "today you will mine:"
+printf "welcome to scryptminerUS!\\n"
 
-case $CURRENCY in
-        "1") printf "digibyte\\n";;
-        "2") printf "lynx\\n";;
-        "3") printf "artbyte\\n";;
-        *) printf "invalid selection, exiting\\n";exit;;
-esac
+function currency {
 
-printf  "where will you mine today?\\n1.)multipool\\n2.)theblocksfactory\\n"
-read -r POOL
-printf "today you will mine at "
-case $POOL in
-        "1") printf "multipool\\n";;
-        "2") printf "theblocksfactory\\n";;
-        *) printf "invalid selection, exiting.\\n";;
-esac
-if [  "$CURRENCY" != 1 ] && [ "$POOL" == 2 ];  then
-        printf "invalid selection, you can't mine that at theblocksfactory, exiting.\\n" ; exit
-fi
-POOLPORT="foo"
-USERNAME="bar"
+        printf "what will you be mining today?\\n1.)digibyte\\n2.)lynx\\n3.)artbyte\\n4.)litecoin\\n"
+        read -r CURRENCY
+        printf "today you will mine:"
+
+        case $CURRENCY in
+                "1") printf "digibyte\\n";;
+                "2") printf "lynx\\n";;
+                "3") printf "artbyte\\n";;
+                "4") printf "litecoin\\n";;
+                *) printf "invalid selection, try again\\n";currency;;
+        esac
+}
+
+function pool {
+
+        printf  "where will you mine today?\\n1.)multipool\\n2.)theblocksfactory\\n"
+        read -r POOL
+        printf "today you will mine at "
+        case $POOL in
+                "1") printf "multipool\\n";;
+                "2") printf "theblocksfactory\\n";;
+                *) printf "invalid selection, try again\\n"; pool;;
+        esac
+
+
+        if [  "$CURRENCY" != 1 ] && [ "$POOL" == 2 ];  then
+                printf "invalid selection, you can't mine that at theblocksfactory, exiting.\\n" ; exit
+        fi
+}
+
+
 function username {
         if [ "$POOL" == 1 ]; then
                 export USERNAME="typefacechaos1.3"
@@ -34,30 +44,39 @@ function username {
         if [ "$POOL" == 2 ]; then
                 export USERNAME="typefacechaos.1"
         fi
-        echo $USERNAME
 }
-
 
 
 function poolport {
         case $POOL in
                 "1") if [ "$CURRENCY" == 1 ]; then
-                        export POOLPORT="us.multipool.us:3351"
+                export POOLPORT="us.multipool.us:3351"
                 fi ;
                 if [ "$CURRENCY" == 2 ]; then
                         export POOLPORT="us.multipool.us:3348"
                 fi ;
                 if [ "$CURRENCY" == 3 ]; then
-                        export POOLPORT="us.multipool.us:2259"
+                        export POOLPORT="us.multipool.us:3359"
+                fi
+                if [ "$CURRENCY" == 4 ]; then
+                        export POOLPORT="us.multipool.us:3334"
                 fi;;
-
                 "2")
                         export POOLPORT="s1.theblocksfactory.com:9004";;
         esac
 
         echo $POOLPORT
 }
-POOLPORT=$(poolport)
-USERNAME=$(username)
-cd
-./bfgminer/bfgminer --scrypt -o stratum+tcp://"$POOLPORT" -u "$USERNAME" -p x -S all --set GSD:clock=300
+
+function main {
+        if [ -f ~/minerd ]; then
+                currency
+                pool
+                POOLPORT=$(poolport)
+                USERNAME=$(username)
+                cd
+                ./bfgminer/bfgminer --scrypt -o stratum+tcp://"$POOLPORT" -u "$USERNAME" -p x -S all --set GSD:clock=300        else
+                printf "you must have minerd to run this script, please install and try again.\\n"
+        fi
+}
+main
